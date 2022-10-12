9b8374f645040ca96dcb25ab159cca67c9a16e36
MohamedHossamElden <mohamedhossamelden2009@gmail.com>
2022-10-12 14:28:43 +0200

    finish

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 class login{
-    constructor(emailIn, passIn, showBtn, image){
+    constructor(emailIn, btn, passIn, showBtn, image){
         this.image = document.getElementById('image');
         
         this.emailIn = document.getElementById('inp1');
 
         this.passIn = document.getElementById('inp2');
         this.showBtn = document.getElementById('showBtn')
+
+        this.btn = document.getElementById('btn')
     }
 
     email(){
@@ -19,6 +21,10 @@ class login{
     show(){
         this.image.src = './imgs/img4.png';
     }
+
+    loginB(){
+        this.image.src = './imgs/img2.png'
+    }
 }
 
 const loginObj = new login();
@@ -26,3 +32,5 @@ const loginObj = new login();
 loginObj.emailIn.onclick = function (){loginObj.email()};
 loginObj.passIn.onclick = function (){loginObj.pass()};
 loginObj.showBtn.onclick = function (){loginObj.show()};
+
+loginObj.btn.onclick = function (){loginObj.loginB()};
