9c475f9dc4a0e1062a8543db4da0f69b9d49b015
MohamedHossamElden <mohamedhossamelden2009@gmail.com>
2022-10-12 14:06:07 +0200

    begin coding

---
+++ app.js
@@ -0,0 +1,18 @@
+class login{
+    constructor(emailIn, passIn, showBtn, image){
+        this.image = document.getElementById('image');
+        
+        this.emailIn = document.getElementById('inp1');
+
+        this.passIn = document.getElementById('inp2');
+        this.showBtn = document.getElementById('showBtn')
+    }
+
+    email(){
+        this.image.src = '../imgs/img1.png';
+    }
+}
+
+const loginObj = new login();
+
+loginObj.emailIn.onclick = function (){loginObj.email()};
